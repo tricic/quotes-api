a8ca0af0efe10fe7f9e9b1a5d2a510b87e95cc11
Ismar <ismar.tricic@gmail.com>
2020-10-09 15:54:21 +0200

    Initial commit, Passport, Quote model, migration and factory

---
+++ database/migrations/2020_10_09_131134_create_quotes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->string('quote');
+            $table->string('author')->nullable();
+            $table->string('category')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->index('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}
