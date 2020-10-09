ed153eca3a92c968c441e9f81a9b26a08a86e28b
Ismar <ismar.tricic@gmail.com>
2020-10-09 16:14:41 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Quote;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::find(1);
+
+        if (!$user)
+        {
+            $user = User::factory()->create([
+                'email' => 'test@mail.net'
+            ]);
+        }
+
+        Quote::factory()->count(64)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
