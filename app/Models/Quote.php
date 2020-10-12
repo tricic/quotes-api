403f8fe29aecf8e41608659c2c3904ef2b5b463c
Ismar <ismar.tricic@gmail.com>
2020-10-12 15:12:41 +0200

    Update Quote.php

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Quote extends Model
 {
@@ -15,4 +16,9 @@ class Quote extends Model
         'author',
         'category'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
